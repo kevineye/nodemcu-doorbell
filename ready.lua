cdc64cdb44e00c700ef810a5c761cf03a6adde28
Kevin Eye <eye@buffalo.edu>
2016-12-15 11:40:08 -0500

    added multi-module semaphore support

--- ready.lua
+++ ready.lua
@@ -1,11 +1,13 @@
 local MODULE = 'ready'
+local log = require 'log'
 
-local ready = {}
+ready = {}
 ready.is_ready      = false
+ready.counter       = 1
 ready.TIMER         = 2
 
 gpio.mode(PIN_LED, gpio.OUTPUT)
-tmr.alarm(ready.TIMER, 3000, tmr.ALARM_AUTO, function()
+tmr.alarm(ready.TIMER, 1000, tmr.ALARM_AUTO, function()
     if (ready.is_ready) then
         tmr.unregister(ready.TIMER)
     else
@@ -13,8 +15,17 @@ tmr.alarm(ready.TIMER, 3000, tmr.ALARM_AUTO, function()
     end
 end)
 
+function ready.not_ready()
+    ready.counter = ready.counter + 1
+    log.trace(MODULE, 'not_ready; ready counter is now '.. ready.counter)
+end
+
 function ready.ready()
-    ready.is_ready = true
+    if ready.counter > 0 then
+        ready.counter = ready.counter - 1
+    end
+    log.trace(MODULE, 'ready; ready counter is now '.. ready.counter)
+    ready.is_ready = ready.counter == 0
 end
 
 return ready
