7d1c220c3ccb9dfe865f35342c946ae0e56fe7f5
Kevin Eye <eye@buffalo.edu>
2016-12-18 14:39:31 -0500

    shrank memory footprint of ready module by 2/3

--- ready.lua
+++ ready.lua
@@ -1,31 +1,13 @@
-local MODULE = 'ready'
-local log = require 'log'
-
-ready = {}
-ready.is_ready      = false
-ready.counter       = 1
-ready.TIMER         = 2
+ready = 1
+local TIMER         = 2
 
 gpio.mode(PIN_LED, gpio.OUTPUT)
-tmr.alarm(ready.TIMER, 3000, tmr.ALARM_AUTO, function()
-    if (ready.is_ready) then
-        tmr.unregister(ready.TIMER)
+tmr.alarm(TIMER, 3000, tmr.ALARM_AUTO, function()
+    if (ready <= 0) then
+        tmr.unregister(TIMER)
     else
         gpio.serout(PIN_LED, gpio.LOW, { 50000, 50000 }, 3, 1)
     end
 end)
-
-function ready.not_ready()
-    ready.counter = ready.counter + 1
-    log.trace(MODULE, 'not_ready; ready counter is now '.. ready.counter)
-end
-
-function ready.ready()
-    if ready.counter > 0 then
-        ready.counter = ready.counter - 1
-    end
-    log.trace(MODULE, 'ready; ready counter is now '.. ready.counter)
-    ready.is_ready = ready.counter == 0
-end
 
 return ready
